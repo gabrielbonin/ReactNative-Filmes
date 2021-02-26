e855b085847f3ce529a686a27e6bb8c417e9f3fd
gabriel bonin <gabrielmatallo1998@gmail.com>
2021-02-26 18:08:48 -0300

    estilização da lista de filmes

--- src/filmes/index.js
+++ src/filmes/index.js
@@ -4,24 +4,58 @@ import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 class Filmes extends Component{
   render(){
     return(
-      <View>
-
+    <View>
       <View style={styles.card}>
         <Text style={styles.titulo}>
           {this.props.data.nome}</Text>
           <Image source={{uri: this.props.data.foto}} style={styles.capa}></Image>
-      </View>
       <View style={styles.Areabotao}>
         <TouchableOpacity style={styles.botao}>
           <Text style={styles.botaoTexto}>Leia mais..</Text>
         </TouchableOpacity>
+        </View>
       </View>
-      </View>
+    </View>
     )
   }
 }
 export default Filmes;
 
 const styles = StyleSheet.create({
+  titulo:{
+    fontSize: 20,
+    padding: 15
+  },
+  card:{
+    shadowColor: '#000',
+    backgroundColor: '#fff',
+    shadowOffset:{width: 0, height: 1},
+    shadowOpacity: 0.8,
+    margin: 15,
+    shadowRadius: 5,
+    borderRadius: 5,
+    elevation: 4
+  },
+  capa:{
+    height: 250,
+    zIndex: 2
+  },
+  Areabotao:{
+    alignItems: 'flex-end',
+    marginTop: -40,
+    zIndex: 9
+  },
+  botao:{
+    width: 100,
+    backgroundColor: '#09A6FF',
+    opacity: 1,
+    padding: 8,
+    borderTopLeftRadius: 5,
+    borderBottomLeftRadius: 5
+  },
+  botaoTexto:{
+    textAlign: 'center',
+    color: '#fff'
+  }
 
 });
